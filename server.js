c9ed9e5e0587885ee602eb6ae9fd9d39e6fe422c
Evyatar Amitay <evyatron@gmail.com>
2016-01-30 23:29:31 +0000

    add timestamps to server logs

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var fs          = require('fs');
 
-
-var PORT = process.env.PORT || 3000;
-var IP   = process.env.IP   || '0.0.0.0';
-
 var app = express(); 
 
 
@@ -15,62 +11,22 @@ var routerClient = express.Router();
 routerClient.use(express.static(path.resolve(__dirname, 'client')));
 app.use('/', routerClient);
 
-
-
 // API routing - for editor work
 var routerAPI = express.Router();
 routerAPI.use(bodyParser.urlencoded({ extended: false }));
 routerAPI.use(bodyParser.json());
+routerAPI.get('/game/:game_id', getGame);
+routerAPI.post('/game/:game_id', updateGame);
+app.use('/api', routerAPI);
 
-function getGamePath(gameId) {
-  return path.resolve(__dirname, 'client/data/' + gameId + '.json');
-}
-
-function apiError(res, err) {
-  console.warn('Error in APi', err);
-  
-  res.json({
-    'error': err
-  });
-}
-
-function apiSaveFile(res, path, content) {
-  fs.access(path, fs.W_OK, function onAccess(err) {
-    if (err) {
-      apiError(res, err);
-      return;
-    }
-
-    fs.writeFile(path, content, 'utf8', function onFileWrite(err, data) {
-      if (err) {
-        apiError(res, err);
-      } else {
-        res.json({
-          'success': true
-        });
-      }
-    });
-  });
-}
-
-function apiPrintJSON(res, path) {
-  fs.readFile(path, 'utf8', function onFileRead(err, data) {
-    if (err) {
-      apiError(res, err);
-      return;
-    }
-    
-    res.json(JSON.parse(data));
-  });
-}
-
-// Get a game
-routerAPI.get('/game/:game_id', function onRequestRoot(req, res) {
+function getGame(req, res) {
+  console.log('[API][Game|' + req.params.game_id + '] Get');
   apiPrintJSON(res, getGamePath(req.params.game_id));
-});
+}
 
-// Update a game
-routerAPI.post('/game/:game_id', function onRequestRoot(req, res) {
+function updateGame(req, res) {
+  console.log('[API][Game|' + req.params.game_id + '] Save');
+  
   var content = req.body;
   
   if (content) {
@@ -88,12 +44,80 @@ routerAPI.post('/game/:game_id', function onRequestRoot(req, res) {
   }
   
   apiSaveFile(res, getGamePath(req.params.game_id), content);
-});
+}
 
-app.use('/api', routerAPI);
+// helpers
 
+function getGamePath(gameId) {
+  return path.resolve(__dirname, 'client/data/' + gameId + '.json');
+}
+
+function apiError(res, err) {
+  console.warn('Error in APi', err);
+  
+  res.json({
+    'error': err
+  });
+}
+
+function apiSaveFile(res, path, content) {
+  fs.access(path, fs.W_OK, function onAccess(err) {
+    if (err) {
+      apiError(res, err);
+    } else {
+      fs.writeFile(path, content, 'utf8', function onFileWrite(err, data) {
+        if (err) {
+          apiError(res, err);
+        } else {
+          res.json({
+            'success': true
+          });
+        }
+      });
+    }
+  });
+}
+
+function apiPrintJSON(res, path) {
+  fs.readFile(path, 'utf8', function onFileRead(err, data) {
+    if (err) {
+      apiError(res, err);
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+}
+
+
+(function addTimestampsToLogs() {
+  var log = console.log;
+  var info = console.info;
+  var warn = console.warn;
+  
+  console.log = function(){ log.apply(this, [getDate()].concat([].slice.call(arguments))); };
+  console.info = function(){ info.apply(this, [getDate()].concat([].slice.call(arguments))); };
+  console.warn = function(){ warn.apply(this, [getDate()].concat([].slice.call(arguments))); };
+  
+  function getDate() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var ms = date.getMilliseconds();
+    
+    (h < 10) && (h = '0' + h);
+    (m < 10) && (m = '0' + m);
+    (s < 10) && (s = '0' + s);
+    (ms < 10) && (ms = '00' + s);
+    (ms < 100) && (ms = '0' + s);
+    
+    return h + ':' + m + ':' + s + '.' + ms;
+  }
+}());
 
 
 // Run the server
+var PORT = process.env.PORT || 3000;
+var IP   = process.env.IP   || '0.0.0.0';
 app.listen(PORT, IP);
 console.log('Server running on ' + IP + ':' + PORT + '...');
